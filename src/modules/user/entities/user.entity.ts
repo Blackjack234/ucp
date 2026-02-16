d7854f5d17964ff370d4890a4e7cb04f7b868848
Blackjack234 <paulanirban390@gmail.com>
2026-02-16 18:51:59 +0530

    changes in user module.

---
+++ src/modules/user/entities/user.entity.ts
@@ -0,0 +1,31 @@
+import { Entity, Column, PrimaryGeneratedColumn, AfterInsert, AfterUpdate, AfterRemove } from "typeorm";
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    email: string;
+
+    @Column()
+    password: string
+
+    @AfterInsert()
+    logInsert(){
+        console.log('Insert received with id',this.id);
+        
+    }
+
+    @AfterUpdate()
+    logUpdate(){
+        console.log('Update completed on id ', this.id);
+        
+    }
+
+    @AfterRemove()
+    logRemove(){
+        console.log('Removed data with id',this.id);
+        
+    }
+}
--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,4 +1,37 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query, ValidationPipe } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { UserService } from './user.service';
+import { UpdateUser } from './dtos/update-user.dto';
 
-@Controller('user')
-export class UserController {}
+@Controller('auth')
+export class UserController {
+    constructor(private userService:UserService){}
+
+    @Post('signup')
+    createUser(@Body(new ValidationPipe({transform:true})) body:CreateUserDto){
+        console.log(body);
+        
+        return this.userService.CreateUserDto(body.email,body.password);
+    }
+
+    @Get('/:id')
+    findOneUser(@Param('id') id : string){
+       return this.userService.findOne(parseInt(id))
+    }
+
+    @Get()
+    findUsers(@Query('email') email:string){
+     return this.userService.find(email)
+    }
+
+    @Delete('/:id')
+    removeUser(@Param('id') id:string)
+    {
+     return this.userService.remove(parseInt(id));
+    }
+
+    @Patch("/:id")
+   async updateUSer(@Body(new ValidationPipe({transform:true})) body:UpdateUser,@Param('id') id:string){
+      return await this.userService.update(parseInt(id),body)
+    }
+}
