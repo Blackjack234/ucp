a884e83b858542c3e999093161df745e76e62a4d
Blackjack234 <paulanirban390@gmail.com>
2026-02-19 17:05:49 +0530

    push

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+const cookieSession = require('cookie-session')
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: true,
+    credentials: true
+  });
+
+  app.use(cookieSession({
+    name: 'session',
+    keys: ['anirban1234'],
+    maxAge: 24 * 60 * 60 * 1000,
+    httpOnly: true,
+    sameSite: 'lax'
+  }));
+
   app.useGlobalPipes(new ValidationPipe({
-    whitelist:true
-  }))
+    whitelist: true,
+    transform: true
+  }));
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Query, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query, ValidationPipe, Session } from '@nestjs/common';
 import { Serialize } from 'src/interceptor/serialize.interceptor';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUser } from './dtos/update-user.dto';
@@ -6,45 +6,55 @@ import { UserDto } from './dtos/user.dto';
 import { UserService } from './user.service';
 import { AuthService } from './auth.service';
 @Controller('auth')
-@Serialize(UserDto)
+// @Serialize(UserDto)
 export class UserController {
-    constructor(private userService:UserService,private authService : AuthService){}
+    constructor(private userService: UserService, private authService: AuthService) { }
 
     @Post('signup')
-    createUser(@Body(new ValidationPipe({transform:true})) body:CreateUserDto){
+    createUser(@Body(new ValidationPipe({ transform: true })) body: CreateUserDto) {
         console.log(body);
-        
-        return this.authService.signup(body.email,body.password);
+
+        return this.authService.signup(body.email, body.password);
     }
 
     @Post('signin')
-    async signin(@Body(new ValidationPipe({transform:true})) body:CreateUserDto){
-     return await this.authService.signin(body.email,body.password)
+    async signin(@Body(new ValidationPipe({ transform: true })) body: CreateUserDto) {
+        return await this.authService.signin(body.email, body.password)
     }
 
     @Get('/:id')
-    findOneUser(@Param('id') id : string){
-       return this.userService.findOne(parseInt(id))
+    findOneUser(@Param('id') id: string) {
+        return this.userService.findOne(parseInt(id))
     }
-    
 
-  
+
+
     @Get()
-    findUsers(@Query('email') email:string){
+    findUsers(@Query('email') email: string) {
 
         console.log('request handler is running.');
-        
-     return this.userService.find(email)
+
+        return this.userService.find(email)
     }
 
     @Delete('/:id')
-    removeUser(@Param('id') id:string)
-    {
-     return this.userService.remove(parseInt(id));
+    removeUser(@Param('id') id: string) {
+        return this.userService.remove(parseInt(id));
     }
 
     @Patch("/:id")
-   async updateUSer(@Body(new ValidationPipe({transform:true})) body:UpdateUser,@Param('id') id:string){
-      return await this.userService.update(parseInt(id),body)
+    async updateUSer(@Body(new ValidationPipe({ transform: true })) body: UpdateUser, @Param('id') id: string) {
+        return await this.userService.update(parseInt(id), body)
+    }
+
+    @Get("color/:color")
+    async setColor(@Param('color') color: string, @Session() session: Record<string,any>) {
+        session.color = color;
+        return { stored: color };
+    }
+
+    @Get('colors')
+    async getColor(@Session() session: Record<string,any>) {
+        return { color: session.color ?? null };
     }
 }
